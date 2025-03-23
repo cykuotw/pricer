30e6c7eb1a175928e9cee6daa38a59d802020227
cykuo <cykuotw45@gmail.com>
2025-03-23 14:02:11 -0400

    feat: implement line chart for displaying prices

--- frontend/src/components/chart.tsx
+++ frontend/src/components/chart.tsx
@@ -1,17 +1,43 @@
 import { useEffect, useState } from "react";
+
 import { API_URL } from "../configs/config";
 import { useTicker } from "../contexts/SelectedTickerContext";
 
+import { Line } from "react-chartjs-2";
+import {
+    Chart as ChartJS,
+    LineElement,
+    PointElement,
+    CategoryScale,
+    LinearScale,
+    Title,
+    Tooltip,
+    Legend,
+    ChartData,
+    ChartOptions,
+} from "chart.js";
+
+ChartJS.register(
+    LineElement,
+    PointElement,
+    CategoryScale,
+    LinearScale,
+    Title,
+    Tooltip,
+    Legend
+);
+
 interface Prices {
     ticker: string;
-    t0: string;
     prices: string[];
+    times: string[];
 }
 
 export default function Chart() {
     const { selectedTicker } = useTicker();
 
-    const [prices, setPrices] = useState<string[]>([]);
+    const [prices, setPrices] = useState<number[]>([]);
+    const [times, setTimes] = useState<string[]>([]);
 
     useEffect(() => {
         const fetchHistorPrices = async () => {
@@ -31,21 +57,53 @@ export default function Chart() {
             }
 
             const prices: Prices = await response.json();
-            setPrices(prices.prices);
+            setPrices(prices.prices.map((p) => parseFloat(p)));
+            setTimes(
+                prices.times.map((t) => {
+                    return new Date(t).toLocaleTimeString("en-CA", {
+                        hour: "2-digit",
+                        minute: "2-digit",
+                        hour12: false,
+                    });
+                })
+            );
         };
 
         fetchHistorPrices();
     }, [selectedTicker]);
 
-    // TODO: Price Chart
+    const data: ChartData<"line"> = {
+        labels: times,
+        datasets: [
+            {
+                label: "Price",
+                data: prices,
+                fill: false,
+                borderColor: "rgb(75, 192, 192)",
+                tension: 0.1,
+            },
+        ],
+    };
+
+    const options: ChartOptions<"line"> = {
+        responsive: true,
+        plugins: {
+            legend: { display: true },
+        },
+        scales: {
+            x: {
+                title: { display: true, text: "Time" },
+            },
+            y: {
+                title: { display: true, text: "Price (USD)" },
+            },
+        },
+    };
+
     return (
         <>
-            <h1>{selectedTicker}</h1>
-            <ul>
-                {prices.map((price, index) => {
-                    return <li key={index}>{price}</li>;
-                })}
-            </ul>
+            <h1>{selectedTicker.length !== 0 ? selectedTicker : "---"}</h1>
+            <Line data={data} options={options} />
         </>
     );
 }
